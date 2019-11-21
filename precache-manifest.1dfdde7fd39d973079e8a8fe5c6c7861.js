b14cc8301f2f278da89d36c680c2f075e097af5d
Sergey <lobalmax@gmail.com>
2019-11-21 07:43:47 +0200

    Updates

---
+++ precache-manifest.1dfdde7fd39d973079e8a8fe5c6c7861.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a081f6c4db87ba5bff0b84a0d461253f",
+    "url": "/itunes-restapi/index.html"
+  },
+  {
+    "revision": "dc618b18470b022018d9",
+    "url": "/itunes-restapi/static/css/main.bdcb1e06.chunk.css"
+  },
+  {
+    "revision": "2815b5c3e6fa445f71f7",
+    "url": "/itunes-restapi/static/js/2.dbba3fbb.chunk.js"
+  },
+  {
+    "revision": "dc618b18470b022018d9",
+    "url": "/itunes-restapi/static/js/main.d7a772d9.chunk.js"
+  },
+  {
+    "revision": "174382e4b631718a96ab",
+    "url": "/itunes-restapi/static/js/runtime-main.89634524.js"
+  }
+]);
